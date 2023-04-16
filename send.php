a1a299caa7a5e86df64698db6e9c34a0a331ad2b
Charlize Isidro <isidroca@students.nu-baliwag.edu.ph>
2023-04-16 22:35:34 +0800

    true na dis, gumagagna na

--- send.php
+++ send.php
@@ -1,40 +1,5 @@
 
 <?php
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\Exception;
- 
-// require 'PHPMailer/src/Exception.php';
-// require 'PHPMailer/src/PHPMailer.php';
-// require 'PHPMailer/src/SMTP.php';
-
-
-
-// if(isset($_POST["send"])){
-//     $name = $_POST['name'];
-//     $message = "rawr" . $name . "narecieve ko  na hano!";
-
-
-//     $mail = new PHPMailer(true);
-//     $mail->isSMTP();
-//     $mail->Host = "smtp.hostinger.com";
-//     $mail->SMTPAuth = true;
-//     $mail->Username = "contact@ka-dahon.shop";
-//     $mail->Password = "charlize@15";
-//     $mail->SMTPSecure = "tls";
-//     $mail->Port = '587';
- 
-//     $mail->setFrom('contact@ka-dahon.shop', 'Ka-Dahon');
-//     $mail->isHTML(true);
-//     $email = $_POST['email'];
-//     $mail->addAddress($email);
-//     $mail->isHTML(true);
-//     $mail->Subject = $_POST["subject"];
-//     $mail->Body =($message);
-//     $mail->send();
-
-//     echo "Feedback Successful!";
- 
-// }
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -57,7 +22,7 @@ if(isset($_POST["send"])){
     $mail->Host = 'smtp.hostinger.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'contact@ka-dahon.shop';
-    $mail->Password = 'charlize@15'; //Gmail App Password
+    $mail->Password = 'Charlize@15'; //Gmail App Password
     $mail->SMTPSecure = 'tls';
     $mail->Port = '587';
 
@@ -65,7 +30,7 @@ if(isset($_POST["send"])){
     $mail->setFrom('contact@ka-dahon.shop', 'Ka-Dahon'); //Senders Email
     $mail->addAddress($sender); //Receivers Email
     $mail->isHTML(true);
-    $mail->Subject = "Good Day!";
+    $mail->Subject = "Greeting!";
     $mail->Body = "Thank you for contacting us! We will get back to you shortly. Have a wonderful day " . $name;
     $mail->send();
 
